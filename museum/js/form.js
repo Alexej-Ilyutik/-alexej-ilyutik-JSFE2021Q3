d6fa0428c737134a969333e019ec2dff257eacc9
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2021-10-04 14:42:53 +0300

    fix: change form

--- museum/js/form.js
+++ museum/js/form.js
@@ -1,11 +1,19 @@
-let ticketsForm = document.getElementById("ticketsForm"),
+let formView = document.body.querySelector(".form__show"),
+  form = document.body.querySelector(".wrapper__form"),
   buttonForm = document.getElementById("buttonForm"),
   closeButton = document.getElementById("close");
 
 buttonForm.onclick = function () {
-  ticketsForm.style.display = "block";
+  form.classList.remove("wrapper__form-hidden");
+  formView.classList.remove("form__hidden");
 };
 
 closeButton.onclick = function () {
-  ticketsForm.style.display = "none";
+  form.classList.add("wrapper__form-hidden");
+  formView.classList.add("form__hidden");
+};
+
+formView.onclick = function () {
+  form.classList.add("wrapper__form-hidden");
+  formView.classList.add("form__hidden");
 };
