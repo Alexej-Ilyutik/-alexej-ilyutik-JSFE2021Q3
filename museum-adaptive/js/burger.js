95f5c5b85e45bc06d0561bee087c33b3636fbbce
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2021-10-05 23:01:06 +0300

    feat: add section Visiting 1024px

--- museum-adaptive/js/burger.js
+++ museum-adaptive/js/burger.js
@@ -2,8 +2,10 @@ const headerMenu = document.querySelector(".header__menu");
 
 if (headerMenu) {
   const headerNavMenu = document.querySelector(".header__nav-menu");
+  const welcomeSection = document.querySelector(".Welcome__section");
   headerMenu.addEventListener("click", function (e) {
     headerMenu.classList.toggle("_active");
     headerNavMenu.classList.toggle("_active");
+    welcomeSection.classList.toggle("_active");
   });
 }
