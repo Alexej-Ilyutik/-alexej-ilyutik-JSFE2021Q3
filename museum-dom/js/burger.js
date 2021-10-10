979c3129561ca6244ef3cc0a35b3a3c6c9897e46
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2021-10-10 15:51:13 +0300

    fix: burger-menu

--- museum-dom/js/burger.js
+++ museum-dom/js/burger.js
@@ -1,11 +1,29 @@
 const headerMenu = document.querySelector(".header__menu");
+const headerNavMenu = document.querySelector(".header__nav-menu");
+const welcomeSection = document.querySelector(".Welcome__section");
+const headerItemMenu = document.querySelectorAll(".header__item-menu");
+const burgerMenuOverlay = document.querySelector(".burger-menu-overlay");
 
 if (headerMenu) {
-  const headerNavMenu = document.querySelector(".header__nav-menu");
-  const welcomeSection = document.querySelector(".Welcome__section");
   headerMenu.addEventListener("click", function (e) {
     headerMenu.classList.toggle("_active");
     headerNavMenu.classList.toggle("_active");
     welcomeSection.classList.toggle("_active");
+    burgerMenuOverlay.classList.toggle("_active");
   });
 }
+
+headerItemMenu.forEach((n) => n.addEventListener("click", closeMenu));
+function closeMenu() {
+  headerMenu.classList.remove("_active");
+  headerNavMenu.classList.remove("_active");
+  welcomeSection.classList.remove("_active");
+  burgerMenuOverlay.classList.remove("_active");
+}
+
+burgerMenuOverlay.onclick = function () {
+  headerMenu.classList.remove("_active");
+  headerNavMenu.classList.remove("_active");
+  welcomeSection.classList.remove("_active");
+  burgerMenuOverlay.classList.remove("_active");
+};
