22501ac37f85e17035385720fc61ed2b8a2a80e9
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2021-10-23 18:41:32 +0300

    feat: add slider

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -2,8 +2,9 @@ const TIME = document.querySelector(".time");
 const DATE = document.querySelector(".date");
 const GREETING = document.querySelector(".greeting");
 const NAME = document.querySelector(".name");
-
-
+const BODY = document.querySelector(".body");
+const SLIDE_NEXT = document.querySelector(".slide-next");
+const SLIDE_PREV = document.querySelector(".slide-prev");
 
 // Date and Time
 
@@ -12,6 +13,7 @@ function showTime() {
   const currentTime = date.toLocaleTimeString();
   TIME.textContent = currentTime;
   showDate();
+  getTimeOfDay();
   setTimeout(showTime, 1000);
 }
 showTime();
@@ -33,31 +35,28 @@ function showDate() {
     "Sunday",
   ];
   let dayWeek = date.getDay();
-  DATE.textContent = days[dayWeek-1] + ", " + currentDate;
+  DATE.textContent = days[dayWeek - 1] + ", " + currentDate;
 }
-
 
 // Greeting
 
-function getTimeOfDay(){
+function getTimeOfDay() {
   const date = new Date();
   const hours = date.getHours();
   if (hours >= 6 && hours < 12) {
-      return "morning"
-    } else if(hours >= 12 && hours < 18) {
-        return "afternoon"
-    }else if (hours >= 18 && hours < 24){
-        return "evening";
-    }else{
-        return "night";
-    }
-
+    return "morning";
+  } else if (hours >= 12 && hours < 19) {
+    return "afternoon";
+  } else if (hours >= 19 && hours < 24) {
+    return "evening";
+  } else {
+    return "night";
+  }
 }
 
 const timeOfDay = getTimeOfDay();
-const greetingText = `Good ${timeOfDay}`;
+const greetingText = `Good ${timeOfDay},`;
 GREETING.textContent = greetingText;
-
 
 function setLocalStorage() {
   localStorage.setItem("name", NAME.value);
@@ -71,3 +70,52 @@ function getLocalStorage() {
 }
 window.addEventListener("load", getLocalStorage);
 
+// Slider
+
+function getRandomNum(min, max) {
+  min = 1;
+  max = 20;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let randomNum = getRandomNum();
+
+function setBg() {
+  const img = new Image();
+  let bgNum = String(randomNum).padStart(2, "0");
+  img.src =
+    "https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/" +
+    `${timeOfDay}` +
+    "/" +
+    `${bgNum}` +
+    ".jpg";
+  
+  img.onload = () => {
+    BODY.style.backgroundImage =
+      "url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/" +
+      `${timeOfDay}` +
+      "/" +
+      `${bgNum}` +
+      ".jpg')";
+  };
+}
+setBg();
+
+function getSlideNext() {
+  if (randomNum === 20) {
+    randomNum = 0;
+  }
+  randomNum++;
+  setBg();
+}
+
+function getSlidePrev() {
+  if (randomNum === 1) {
+    randomNum = 21;
+  }
+  randomNum--;
+  setBg();
+}
+
+SLIDE_NEXT.addEventListener("click", getSlideNext);
+SLIDE_PREV.addEventListener("click", getSlidePrev);
