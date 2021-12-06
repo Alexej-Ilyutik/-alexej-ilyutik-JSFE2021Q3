4fcb69ca3adc60e52b6b09c0520267d5142d710c
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2021-12-06 14:50:32 +0300

    feat: add ESLint

--- migration-to-ts/webpack.config.js
+++ migration-to-ts/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/index.js'),
@@ -24,7 +25,7 @@ const baseConfig = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '/dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -32,6 +33,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new ESLintPlugin(),
     ],
 };
 
