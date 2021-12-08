06d101f2555eb3165be19ab9533812a6f65b5c4d
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2021-12-08 22:36:46 +0300

    fix: change link for work with API

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'b97faed1f6d541d2848c762baf9a7737', // получите свой ключ https://newsapi.org/
         });
     }
