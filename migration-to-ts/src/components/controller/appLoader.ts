6bc807a314c900c66d3ef86c9b0ddd8f74b8f0e2
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2021-12-08 22:43:25 +0300

    refactor: delete comment

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://nodenews.herokuapp.com/', {
-            apiKey: 'b97faed1f6d541d2848c762baf9a7737', //  https://newsapi.org/
+            apiKey: 'b97faed1f6d541d2848c762baf9a7737', // https://newsapi.org/
         });
     }
 }
