e4a9c44343f5fefd33acd118360474e1e5e42bfd
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2021-12-20 22:05:36 +0300

    feat: add new bg picture

--- christmas-task/src/index.ts
+++ christmas-task/src/index.ts
@@ -11,24 +11,24 @@ import Footer from './template/Footer/footer';
 const header = document.querySelector('header');
 header.append(Header);
 
-// const main = document.querySelector('main');
-// main.append(Main);
+const main = document.querySelector('main');
+main.append(Main);
 
 const main1 = document.querySelector("main");
-main1.append(Main1);
+// main1.append(Main1);
 
 const footer = document.querySelector('footer');
 footer.append(Footer);
 
-// const MAIN_BTN = document.getElementById("btn");
+const MAIN_BTN = document.getElementById("btn");
 
-// function showMain1() {
-//   Main.remove();
-//   main.appendChild(Main1);
-//   console.log("Hello World!");
-// }
+function showMain1() {
+  Main.remove();
+  main.appendChild(Main1);
+  console.log("Hello World!");
+}
 
-// MAIN_BTN.onclick = () => {
-//   showMain1();
-// };
+MAIN_BTN.onclick = () => {
+  showMain1();
+};
 
